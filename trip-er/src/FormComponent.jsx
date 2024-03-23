47e8a28e91bbe169cec4d8fa5cddda7370903669
prudhvi <96768786+prudhvi3105@users.noreply.github.com>
2024-03-23 17:00:54 +0530

    Update FormComponent.jsx

--- trip-er/src/FormComponent.jsx
+++ trip-er/src/FormComponent.jsx
@@ -95,7 +95,7 @@ function FormComponent() {
   return (
     <div className="maindiv1">
       <DropdownMenu
-        label="Travel Day"
+        label="Travel Days"
         options={['Travel day', '1 Day', '2 Day', '3 Day', '4 Day', '5 Day', '6 Day', '7 Day']}
         value={formData.selectedTravelDay}
         onChange={(value) => handleInputChange('selectedTravelDay', value)}
